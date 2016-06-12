2bc72753f1d1e76ce6bcb863276537110957af56
Mohamed Hayibor <mohamedhayibor@gmail.com>
2016-06-12 18:37:36 -0400

    refactor commands

--- cli.js
+++ cli.js
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
-const Configstore = require('configstore');
 const meow = require('meow');
-const pkg = require('./package.json');
-const cityIsValid = require('./lib/city-is-valid');
 const getStationData = require('./lib/get-station-data');
-
-const conf = new Configstore(pkg.name);
+const commands = require('./lib/commands')
 
 let cli = meow(`
 Usage: bikes                          # Display available bikes/slots for your default station
@@ -26,53 +22,6 @@ Usage: bikes                          # Display available bikes/slots for your d
   }
 });
 
-let serviceId = conf.get('city');
-
-let commands = {
-  'set-city': function(args) {
-    serviceId = args[0];
-    cityIsValid(serviceId)
-      .then(function(isValid) {
-        if (isValid) {
-          conf.set('city', serviceId);
-          console.log(`Your city has been set to ${serviceId}!`);
-        } else {
-          console.log(`Sorry that doesn't seem to be a valid serviceId.`)
-        }
-      })
-      .catch(function(err) {
-        console.log(`Sorry, there was some error.`)
-      });
-  },
-  'station': function(stationIdOrAlias) {
-    stationIdOrAlias = stationIdOrAlias || '';
-    if (!serviceId) {
-      throw new Error('You have not set your serviceId yet');
-    } else {
-      let uri = `http://api.citybik.es/v2/networks/${serviceId}`;
-      return getStationData(uri, stationIdOrAlias);
-    }
-  },
-  'set': function (args) {
-    if (args.length < 2) {
-      throw new Error('You need at least 2 parameters to set default values.')
-    }
-    conf.set(args[0], args[1]);
-  },
-  'get': function(args) {
-    let savedParam = args[0];
-    if ( !conf.get(savedParam) ) {
-      console.log(`
-  You have not saved this parameter yet.
-  use command: $ bikes set <param> <value>
-  `);
-
-    } else {
-      savedParam = conf.get(savedParam);
-      commands.station(savedParam);
-    }
-  }
-};
 
 if (cli.input[0] in commands) {
   commands[cli.input.shift()](cli.input);
